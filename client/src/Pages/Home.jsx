588c69889a257e4422c428735ca9e5f24c10e226
Sharuk <msharuk.b10@gmail.com>
2024-01-03 18:44:06 +0530

    minor changes

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import NavBar from '../Components/navbar/NavBar'
 import {images} from '../assets/data.js';
 import Banner from '../Components/banner/Banner.jsx';
-import FilterCards from '../Components/cards/filterCards.jsx';
+import FilterCards from '../Components/cards/FilterCards.jsx';
 import ProductCard from '../Components/products/ProductCard.jsx';
 import FooterSection from '../Components/footer/FooterSection.jsx';
-import { ToastBar, Toaster } from 'react-hot-toast';
+import {  Toaster } from 'react-hot-toast';
 
 const Home = () => {
   return (
